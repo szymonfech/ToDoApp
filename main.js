1087c4164676a42eeddd884419a196fff79d8d80
szymonfech <szymon.fechner@gmail.com>
2018-01-17 23:41:19 +0100

    added dbclick to delete item

--- main.js
+++ main.js
@@ -1,23 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
+    //zmienne
     var button = document.querySelector("button");
     var buttonReset = document.querySelector(".btn-reset");
     var input = document.querySelector("input");
-    var taskList = document.queryCommandEnabled("li")
+    //odswieżenie strony
     buttonReset.addEventListener("click", function () {
         location.reload()
     })
+    //Po kliknieciu sprawdzenie czy jest wiecej niz 50 znakow lub czy nic nie zostalo wpisane i alert
     button.addEventListener("click", function () {
 
         var inputValue = input.value;
         if (inputValue.length > 50 || inputValue == "") {
             alert("Zadanie jest za długie, lub nie zawiera zawartości. Zmień to !")
-        } else {
+        }
+        // jedno klikniecie podkreslenie
+        else {
             var ul = document.querySelector("ul");
             var li = document.createElement("li");
             ul.appendChild(li).innerHTML = "&#9733;" + "&nbsp;" + inputValue;
             ul.appendChild(li).addEventListener("click", function () {
                 this.classList.add("deleted");
             })
+            // dwa klikniecia usunięcie
+            ul.appendChild(li).addEventListener("dblclick", function () {
+                ul.removeChild(this)
+            })
+
         }
     });
 })
